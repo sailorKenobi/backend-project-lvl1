b5c6e3aa82cbfdf454e6bb2e44e50f1ded64654c
Max <sailorkenobi@gmail.com>
2022-02-20 13:23:25 +0300

    fix lint error

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { getInput } from '../src/cli.js';
+import getInput from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const name = getInput('May I have you name?');
