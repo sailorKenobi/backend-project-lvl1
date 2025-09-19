e4ddf572a34b21639f0645cc72b8c68da55fd397
Max <38960498+sailorKenobi@users.noreply.github.com>
2025-09-19 20:31:11 +0300

    step 9: add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,22 @@
+import { runQuestionsGame, getRandomInt } from '../index.js'
+
+export const primeGame = () => {
+  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+  runQuestionsGame(gameDescription, primePlay)
+}
+
+const primePlay = () => {
+  const randNumber = getRandomInt(100)
+  const res = {
+    question: randNumber,
+    answer: isPrime(randNumber),
+  }
+  return res
+}
+const isPrime = (number) => {
+  for (let i = 2; i <= number / 2; i++) {
+    if (number % i === 0)
+      return 'no'
+  }
+  return 'yes'
+}
